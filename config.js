536255259cd0823204ee78aa730d067b2bb48cef
sherylynn <352281674@qq.com>
2018-10-27 22:41:47 +0800

    we don't need to track config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  // server_origin:'http://127.0.0.1:3456'
-  server_origin: 'your_db_url',
-  server_admin: 'your_db_admin',
-  server_passwd: 'your_db_admin_passwd',
-};
-export { config, };
