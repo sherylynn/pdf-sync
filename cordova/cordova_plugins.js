bedc1b49cd1615e8efa609b1406c8c095c46f0bb
sherylynn <352281674@qq.com>
2019-07-24 20:13:24 +0800

    fix error in open pdf

--- cordova/cordova_plugins.js
+++ cordova/cordova_plugins.js
@@ -174,11 +174,11 @@ module.exports = [
     "runs": true
   }
 ];
-module.exports.metadata = 
+module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-file": "6.0.1"
+  "cordova-plugin-file": "6.0.1",
   "cordova-plugin-androidx": "1.0.2",
   "cordova-plugin-androidx-adapter": "1.1.0"
 };
